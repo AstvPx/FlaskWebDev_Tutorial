ed43f5cf3b3c3521cc0b7e30935f43da2509c682
Phoenix <astvpx@126.com>
2017-07-17 22:35:13 +0800

    Chapter03: Templates.

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from datetime import datetime
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    ua = request.headers.get('User-Agent')
-    return '<p>Flask Web Development. </p><p>User-Agent is : %s .</p>' % ua
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h2>Hello, %s !</h2>' % name
+    return render_template('user.html', name=name)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
-    # app.run(debug=True)
